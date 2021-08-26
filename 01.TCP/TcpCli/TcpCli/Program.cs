44d47dd19198f56411c8e214c6aa9f98459307d0
whyj107 <60024292+whyj107@users.noreply.github.com>
2021-08-26 22:01:31 +0900

    TIL
    Study TCP Basic

---
+++ 01.TCP/TcpCli/TcpCli/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // IP 주소와 포트를 지정하고 TCP 연결
+            TcpClient tc = new TcpClient("localhost", 7000);
+
+            string msg = "Hello World";
+            byte[] buff = Encoding.ASCII.GetBytes(msg);
+
+            // NetworkStream을 얻어옴
+            NetworkStream stream = tc.GetStream();
+
+            // 스트림에 바이트 데이타 전송
+            stream.Write(buff, 0, buff.Length);
+
+            // 서버가 connection을 닫을 때까지 읽는 경우
+            byte[] outbuf = new byte[1024];
+            int nbytes;
+            MemoryStream mem = new MemoryStream();
+            while ((nbytes = stream.Read(outbuf, 0, outbuf.Length)) > 0)
+            {
+                mem.Write(outbuf, 0, nbytes);
+            }
+            byte[] outbytes = mem.ToArray();
+            mem.Close();
+
+            // 스트림과 TcpClient 객체 닫기
+            stream.Close();
+            tc.Close();
+
+            Console.WriteLine(Encoding.ASCII.GetString(outbytes));
+        }
+    }
+}
---
+++ 01.TCP/TcpSrvAsync/TcpSrvAsync/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpSrvAsync
+{
+    class Program
+    {
+        /// <summary>
+        /// http://www.csharpstudy.com/net/article/6 예제
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            AysncEchoServer().Wait();
+        }
+
+        async static Task AysncEchoServer()
+        {
+            // 로컬 포트 7000을 Listen
+            TcpListener listener = new TcpListener(IPAddress.Any, 7000);
+            listener.Start();
+            while (true)
+            {
+                // TcpClient Connection 요청을 받아들여
+                // 서버에서 새 TcpClient 객체를 생성하여 리턴
+                // 비동기 Accept
+                TcpClient tc = await listener.AcceptTcpClientAsync().ConfigureAwait(false);
+
+                // 새 쓰레드에서 처리
+                await Task.Factory.StartNew(AsyncTcpProcess, tc);
+            }
+        }
+
+        async static void AsyncTcpProcess(object o)
+        {
+            TcpClient tc = (TcpClient)o;
+
+            int MAX_SIZE = 1924;
+            // TcpClient 객체에서 NetworkStream을 얻음
+            NetworkStream stream = tc.GetStream();
+
+            // 비동기 수신
+            var buff = new byte[MAX_SIZE];
+            // 클라이언트가 연결을 끊을 때까지 데이터를 수신
+            var nbytes = await stream.ReadAsync(buff, 0, buff.Length).ConfigureAwait(false);
+            if(nbytes > 0)
+            {
+                string msg = Encoding.ASCII.GetString(buff, 0, nbytes);
+                Console.WriteLine($"{msg} at {DateTime.Now}");
+
+                // 비동기 송신
+                await stream.WriteAsync(buff, 0, nbytes).ConfigureAwait(false);
+            }
+
+            // 스트림과 TcpClient 객체
+            stream.Close();
+            tc.Close();
+        }
+
+        /// <summary>
+        /// 수신시 타임아웃 기능 추가 예제
+        /// 수신 Task와 타임아웃 Task를 생성하고 Task.WhenAnu()를 사용하여 2개 중 하나가 완료되면 리턴하도록 설정
+        /// </summary>
+        /// <returns></returns>
+        async static Task AysncEchoServerTimeOut()
+        {
+            int MAX_SIZE = 1924;
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 7000);
+            listener.Start();
+            
+            while (true)
+            {
+                // 비동기 Accept
+                TcpClient tc = await listener.AcceptTcpClientAsync().ConfigureAwait(false);
+
+                NetworkStream stream = tc.GetStream();
+
+                var buff = new byte [MAX_SIZE];
+                var readTask = stream.ReadAsync(buff, 0, buff.Length);
+                var timeoutTask = Task.Delay(10 * 1000); // 10초
+                var doneTask = await Task.WhenAny(timeoutTask, readTask).ConfigureAwait(false);
+
+                // 타임 아웃일 경우
+                if(doneTask == timeoutTask)
+                {
+                    var bytes = Encoding.ASCII.GetBytes("Read Timeout Error");
+                    await stream.WriteAsync(bytes, 0, bytes.Length);
+                }
+                // 타임 아웃이 아닐 경우 == 수신 성공
+                else
+                {
+                    int nbytes = readTask.Result;
+                    if(nbytes > 0)
+                    {
+                        await stream.WriteAsync(buff, 0, nbytes).ConfigureAwait(false);
+                    }
+                }
+
+                stream.Close();
+                tc.Close();
+            }
+        }
+    }
+}
