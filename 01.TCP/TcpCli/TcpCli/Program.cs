2a7b73dc535e0229ec9912ac7cde1f40d4fc5c1e
whyj107 <60024292+whyj107@users.noreply.github.com>
2021-08-31 17:21:36 +0900

    Update Program.cs

--- 01.TCP/TcpCli/TcpCli/Program.cs
+++ 01.TCP/TcpCli/TcpCli/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -9,84 +8,6 @@ namespace TcpCli
 {
     class Program
     {
-        static int i = 0;
-        private static void Test(int idx)
-        {
-            try
-            {
-                // IP 주소와 포트를 지정하고 TCP 연결
-                TcpClient tc = new TcpClient("localhost", 7000);
-
-                string msg = $"{idx}: Hello World";
-                byte[] buff = Encoding.ASCII.GetBytes(msg);
-
-                // NetworkStream을 얻어옴
-                NetworkStream stream = tc.GetStream();
-
-                // 스트림에 바이트 데이타 전송
-                stream.Write(buff, 0, buff.Length);
-                Console.WriteLine($"{idx} at {DateTime.Now}");
-
-                // 서버가 connection을 닫을 때까지 읽는 경우
-                byte[] outbuf = new byte[1024];
-                int nbytes;
-                /*
-                MemoryStream mem = new MemoryStream();
-                while ((nbytes = stream.Read(outbuf, 0, outbuf.Length)) > 0)
-                {
-                    mem.Write(outbuf, 0, nbytes);
-                }
-                byte[] outbytes = mem.ToArray();
-                mem.Close();
-                Console.WriteLine(Encoding.ASCII.GetString(outbytes));
-                */
-                // 스트림과 TcpClient 객체 닫기
-                stream.Close();
-                tc.Close();
-
-            }
-            catch
-            {
-                // 연결 오류 발생
-            }
-
-        }
-
-        private static void SocketTest()
-        {
-            // (1) 소켓 객체 생성 (TCP 소켓)
-            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            // (2) 서버에 연결
-            var ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7000);
-            sock.Connect(ep);
-
-            string cmd = string.Empty;
-            byte[] receiverBuff = new byte[8192];
-
-            Console.WriteLine("Connected... Enter Q to exit");
-
-            // Q 를 누를 때까지 계속 Echo 실행
-            while ((cmd = Console.ReadLine()) != "Q")
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    byte[] buff = Encoding.UTF8.GetBytes($"{i}");
-                    // (3) 서버에 데이타 전송
-                    sock.Send(buff, SocketFlags.None);
-
-                    // (4) 서버에서 데이타 수신
-                    int n = sock.Receive(receiverBuff);
-
-                    string data = Encoding.UTF8.GetString(receiverBuff, 0, n);
-                    Console.WriteLine(data);
-                }
-            }
-
-            // (5) 소켓 닫기
-            sock.Close();
-        }
-
         // 클라이언트 데이터를 비동기식으로 읽기 위한 상태 개체
         public class StateObject
         {
@@ -121,30 +42,50 @@ namespace TcpCli
                 {
                     // 소켓의 원격 끝점을 설정
                     IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+                    
                     // TCP/IP 소켓 생성  
                     Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    // TIME OUT 없애기 위해서 옵션 추가
+                    client.LingerState = new LingerOption(true, 0);
 
-                    // 원격 끝점과 연결  
-                    client.BeginConnect(remoteEP, new AsyncCallback(ConnectCallback), client);
+                    for(int j=0; j<3; j++)
+                    {
+                        if (!client.Connected)
+                        {
+                            connectDone.Reset();
+                            // 원격 끝점과 연결  
+                            client.BeginConnect(remoteEP, new AsyncCallback(ConnectCallback), client);
+                            connectDone.WaitOne();
+                        }
 
-                    connectDone.WaitOne();
+                        if (client.Connected)
+                        {
+                            // 원격 기계에 테스트 데이터 전송  
+                            sendDone.Reset();
+                            Send(client, $"test: {j}<EOF>");
+                            sendDone.WaitOne();
 
-                    // 원격 기계에 테스트 데이터 전송  
-                    Send(client, $"test: {i++}<EOF>");
-                    sendDone.WaitOne();
+                            // 원격 기계에서 응답 수신  
+                            receiveDone.Reset();
+                            Receive(client);
+                            receiveDone.WaitOne();
 
-                    // 원격 기계에서 응답 수신  
-                    Receive(client);
-                    receiveDone.WaitOne();
+                            // 응답 내용 출력  
+                            Console.WriteLine("Response received : {0}", response);
+                            Console.WriteLine("============================================");
+                            response = String.Empty;
 
-                    // 응답 내용 출력  
-                    Console.WriteLine("Response received : {0}", response);
-                    Console.WriteLine("============================================");
+                        }
+
+                    }
+
                     while (client.Connected)
                     {
                         // 소켓 해제
-                        client.Shutdown(SocketShutdown.Both);
+                        client.Disconnect(false);
+                        // client.Shutdown(SocketShutdown.Both);
                         client.Close();
+                        client.Dispose();
                     }
 
                 }
@@ -208,18 +149,28 @@ namespace TcpCli
                         // 데이터가 더 있을 수 있으므로 지금까지 수신한 데이터 저장
                         state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
 
-                        // 나머지 데이터 가져옴
-                        client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
+                        string tmp = state.sb.ToString();
+                        if(tmp.IndexOf("<EOF>") > -1)
+                        {
+                            if (state.sb.Length > 1)
+                            {
+                                response = state.sb.ToString();
+                            }
+
+                            receiveDone.Set();
+                        }
+                        else
+                        {
+                            // 나머지 데이터 가져옴
+                            client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
+                        }
                     }
                     else
                     {
-                        // 모든 데이터가 도착했으므로 응답 변수에 넣어서 확인
-                        if (state.sb.Length > 1)
-                        {
-                            response = state.sb.ToString();
-                        }
-                        // 모든 바이트가 수신되었음을 나타냄
+                        client.Disconnect(true);
+                        response = "데이터 값 없음";
                         receiveDone.Set();
+                        return;
                     }
                 }
                 catch (Exception e)
@@ -261,16 +212,7 @@ namespace TcpCli
 
             public static int Main(String[] args)
             {
-                /*
-                for(int i=0; i<10; i++)
-                {
-                    Test(i);
-                }
-                */
-                // SocketTest();
-
-
-                for(int i=0; i<1000; i++)
+                for(int i=0; i<3; i++)
                 {
                     StartClient();
                 }
