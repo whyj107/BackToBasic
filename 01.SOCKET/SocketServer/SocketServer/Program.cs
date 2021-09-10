eca82ba9a3ea32eefe1d28970dcf06a691645e7b
whyj107 <60024292+whyj107@users.noreply.github.com>
2021-09-11 06:47:35 +0900

    Update

---
+++ 01.SOCKET/SocketServer/SocketServer/Program.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SocketServer
+{
+    class Program
+    {
+        public static Socket listener;
+        private static bool IsListening = true;
+        private static List<Socket> connectedClients = new List<Socket>();
+        private static string ipAddress = "127.0.0.1";
+        private static int port = 11000;
+
+        // 클라이언트 데이터를 비동기식으로 읽기 위한 상태 개체
+        public class StateObject
+        {
+            // 수신 버퍼의 크기
+            public const int BufferSize = 1024;
+            // 수신 버퍼
+            public byte[] buffer = new byte[BufferSize];
+            // 수신 데이터 문자열
+            public StringBuilder sb = new StringBuilder();
+            // 클라이언트 소켓
+            public Socket workSocket = null;
+
+            public System.Timers.Timer timer = new System.Timers.Timer();
+            public double receive_cnt = 10;
+
+            public void CountDown()
+            {
+                if (timer.Enabled)
+                {
+                    timer.Stop();
+                }
+                receive_cnt = 10;
+                timer.Interval = 1000;
+                timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+                timer.Start();
+            }
+            private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+            {
+                Console.WriteLine(DateTime.Now + " : " + receive_cnt );
+                receive_cnt--;
+                if(receive_cnt < 0)
+                {
+                    receive_cnt = 0;
+                    timer.Stop();
+                }
+            }
+        }
+
+        public class AsynchronousSocketListener
+        {
+            // 스레드 신호
+            public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+            public static void StartListening()
+            {
+                // "127.0.0.1" 서버에 연결  
+                IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+
+                // TCP/IP 소켓 생성  
+                listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // 소켓을 로컬 끝점에 바인딩하고 들어오는 연결을 수신
+                try
+                {
+                    // listener.Bind(localEndPoint);
+
+                    // 서버 자체에 존재하는 IP 사용
+                    listener.Bind(new IPEndPoint(IPAddress.Any, port));
+
+                    listener.Listen(100);
+
+                    while (true)
+                    {
+                        // 이벤트를 비신호 상태로 설정  
+                        allDone.Reset();
+
+                        // 연결을 수신하려면 비동기 소켓을 시작 
+                        Console.WriteLine("Waiting for a connection...");
+                        listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+
+                        // 연결이 될 때까지 기다린 후 계속 진행
+                        allDone.WaitOne();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("StartListening\n: " + e.Message);
+                }
+            }
+
+            // IAsyncResult : 비동기적으로 작동할 수 있는 메서드를 포함하는 클래스에 의해 구현됨
+            public static void AcceptCallback(IAsyncResult ar)
+            {
+                try
+                {
+                    // 계속하려면 메인 스레드에 신호를 보냄  
+                    allDone.Set();
+
+                    if (!IsListening)
+                    {
+                        return;
+                    }
+
+                    // 클라이언트 요청을 처리하는 소켓을 가져옴 
+                    Socket l = (Socket)ar.AsyncState;
+                    Socket handler = l.EndAccept(ar);
+
+                    // List에 추가
+                    connectedClients.Add(handler);
+
+                    // 상태 개체 생성
+                    StateObject state = new StateObject();
+                    state.workSocket = handler;
+
+                    // 시간 재기 시작
+                    if (!state.timer.Enabled)
+                    {
+                        state.CountDown();
+                    }
+
+                    // AsyncCallback : 해당 비동기 작업을 완료할 때 호출되는 메서드를 참조합니다.
+                    // public delegate void AsyncCallback(IAsyncResult ar);형식이 사용됨
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("AcceptCallback\n: " + ex.Message);
+                }
+            }
+
+            public static void ReadCallback(IAsyncResult ar)
+            {
+                try
+                {
+                    String content = String.Empty;
+
+                    // 비동기 상태 개체에서 상태 개체 및 처리기 소켓을 검색
+                    StateObject state = (StateObject)ar.AsyncState;
+                    Socket handler = state.workSocket;
+
+                    // 만약 receive_cnt가 0이면(5초 동안 아무것도 받지 못하면) 종료
+                    if (state.receive_cnt == 0)
+                    {
+                        state.timer.Stop();
+
+                        if (connectedClients.Contains(handler))
+                        {
+                            connectedClients.Remove(handler);
+                        }
+
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                        handler.Dispose();
+                        Console.WriteLine("handler 종료");
+                    }
+
+                    // 클라이언트 소켓에서 데이터 읽기
+                    int bytesRead = handler.EndReceive(ar);
+
+                    if (bytesRead > 0)
+                    {
+                        // 더 많은 데이터가 있을 수 있으므로 지금까지 수신한 데이터를 저장 
+                        state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+
+                        // 파일 끝 태그를 확인, 없으면 더 많은 데이터를 읽기
+                        content = state.sb.ToString();
+                        if (content.IndexOf("<EOF>") > -1)
+                        {
+                            // 모든 데이터를 클라이언트에서 읽은 후 콘솔에 표시  
+                            Console.WriteLine("Read {0} bytes from socket. \n Data : {1}", content.Length, content);
+                            // 데이터를 클라이언트로 반향
+                            Send(handler, $"ok: {content}");
+                            state.sb.Clear();
+                            // 시간 다시 세기
+                            state.receive_cnt = 10;
+                        }
+                    }
+                    // Receive를 새로 설정해줘서 다음 값을 받을 수 있도록 설정
+                    handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("ReadCallback\n: " + ex.Message);
+                }
+            }
+
+            private static void Send(Socket handler, String data)
+            {
+                try
+                {
+                    // ASCII 인코딩을 사용하여 문자열 데이터를 바이트 데이터로 변환 
+                    byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+                    // 원격 장치로 데이터 전송을 시작 
+                    handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), handler);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Send\n: " + ex.Message);
+                }
+            }
+
+            private static void SendCallback(IAsyncResult ar)
+            {
+                try
+                {
+                    // 상태 개체에서 소켓을 검색
+                    Socket handler = (Socket)ar.AsyncState;
+
+                    // 원격 장치로 데이터 전송 
+                    int bytesSent = handler.EndSend(ar);
+                    // Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+                    Console.WriteLine("==========================================", bytesSent);
+
+                    // Socket을 닫아버린다.
+                    // handler.Shutdown(SocketShutdown.Both);
+                    // handler.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("SendCallback\n: " + ex.Message);
+                }
+            }
+
+            public static void StopListening()
+            {
+                if(listener != null)
+                {
+                    IsListening = false;
+                    try
+                    {
+                        foreach (Socket socket in connectedClients)
+                        {
+                            if (socket.Connected)
+                            {
+                                socket.Disconnect(false);
+                                socket.Shutdown(SocketShutdown.Both);
+                                socket.Dispose();
+                            }
+                        }
+                        connectedClients.Clear();
+
+                        listener.Close();
+                        listener.Dispose();
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                Thread.Sleep(1000);
+                Console.WriteLine("프로그램을 종료합니다.========================");
+                for (int i=3; i>0; i--)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(1000);
+                }
+            }
+
+            public static void Main(string[] args)
+            {
+                AppDomain.CurrentDomain.ProcessExit += Application_ProcessExit;
+                StartListening();
+            }
+
+            private static void Application_ProcessExit(object sender, EventArgs e)
+            {
+                StopListening();
+            }
+        }
+    }
+}
---
+++ 01.SOCKET/TcpCli/TcpCli/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TcpCli
+{
+    class Program
+    {
+        // 클라이언트 데이터를 비동기식으로 읽기 위한 상태 개체
+        public class StateObject
+        {
+            // 수신 버퍼의 크기
+            public const int BufferSize = 256;
+            // 수신 버퍼
+            public byte[] buffer = new byte[BufferSize];
+            // 수신 데이터 문자열
+            public StringBuilder sb = new StringBuilder();
+            // 클라이언트 소켓
+            public Socket workSocket = null;
+        }
+
+        public class AsynchronousClient
+        {
+            // 포트 번호
+            private const int port = 11000;
+
+            // 수동 리셋 이벤트 인스턴스 신호
+            private static ManualResetEvent connectDone = new ManualResetEvent(false);
+            private static ManualResetEvent sendDone = new ManualResetEvent(false);
+            private static ManualResetEvent receiveDone = new ManualResetEvent(false);
+
+            // 원격 장치의 응답 
+            private static String response = String.Empty;
+
+            public static void StartClient()
+            {
+
+                // 원격 장치 연결 
+                try
+                {
+                    // 소켓의 원격 끝점을 설정
+                    IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+                    
+                    // TCP/IP 소켓 생성  
+                    Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    // TIME OUT 없애기 위해서 옵션 추가
+                    client.LingerState = new LingerOption(true, 0);
+
+                    for(int j=0; j<3; j++)
+                    {
+                        if (!client.Connected)
+                        {
+                            connectDone.Reset();
+                            // 원격 끝점과 연결  
+                            client.BeginConnect(remoteEP, new AsyncCallback(ConnectCallback), client);
+                            connectDone.WaitOne();
+                        }
+
+                        if (client.Connected)
+                        {
+                            // 원격 기계에 테스트 데이터 전송  
+                            sendDone.Reset();
+                            Send(client, $"test: {j}<EOF>");
+                            sendDone.WaitOne();
+
+                            // 원격 기계에서 응답 수신  
+                            receiveDone.Reset();
+                            Receive(client);
+                            receiveDone.WaitOne();
+
+                            // 응답 내용 출력  
+                            Console.WriteLine("Response received : {0}", response);
+                            Console.WriteLine("============================================");
+                            response = String.Empty;
+
+                        }
+
+                    }
+
+                    while (client.Connected)
+                    {
+                        // 소켓 해제
+                        client.Disconnect(false);
+                        // client.Shutdown(SocketShutdown.Both);
+                        client.Close();
+                        client.Dispose();
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            private static void ConnectCallback(IAsyncResult ar)
+            {
+                try
+                {
+                    // 상태 개체에서 소켓을 검색  
+                    Socket client = (Socket)ar.AsyncState;
+
+                    // 연결 완료
+                    client.EndConnect(ar);
+
+                    Console.WriteLine("Socket connected to {0}", client.RemoteEndPoint.ToString());
+
+                    // 연결되었음을 알리는 신호 
+                    connectDone.Set();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            private static void Receive(Socket client)
+            {
+                try
+                {
+                    // 상태 개체 생성  
+                    StateObject state = new StateObject();
+                    state.workSocket = client;
+
+                    // 원격 장치에서 데이터 수신 시작
+                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            private static void ReceiveCallback(IAsyncResult ar)
+            {
+                try
+                {
+                    // 비동기 상태 개체에서 상태 개체 및 클라이언트 소켓을 검색 
+                    StateObject state = (StateObject)ar.AsyncState;
+                    Socket client = state.workSocket;
+
+                    // 원격 장치에서 데이터 수신  
+                    int bytesRead = client.EndReceive(ar);
+
+                    if (bytesRead > 0)
+                    {
+                        // 데이터가 더 있을 수 있으므로 지금까지 수신한 데이터 저장
+                        state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+
+                        string tmp = state.sb.ToString();
+                        if(tmp.IndexOf("<EOF>") > -1)
+                        {
+                            if (state.sb.Length > 1)
+                            {
+                                response = state.sb.ToString();
+                            }
+
+                            receiveDone.Set();
+                        }
+                        else
+                        {
+                            // 나머지 데이터 가져옴
+                            client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReceiveCallback), state);
+                        }
+                    }
+                    else
+                    {
+                        client.Disconnect(true);
+                        response = "데이터 값 없음";
+                        receiveDone.Set();
+                        return;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            private static void Send(Socket client, String data)
+            {
+                // ASCII 인코딩을 사용하여 문자열 데이터를 바이트 데이터로 변환
+                byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+                Console.WriteLine(data);
+
+                // 원격 장치로 데이터 전송 시작
+                client.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), client);
+            }
+
+            private static void SendCallback(IAsyncResult ar)
+            {
+                try
+                {
+                    // 상태 개체에서 소켓을 검색 
+                    Socket client = (Socket)ar.AsyncState;
+
+                    // 원격 장치로 데이터 전송 완료
+                    int bytesSent = client.EndSend(ar);
+                    Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+
+                    // 모든 바이트가 전송되었음을 나타냄
+                    sendDone.Set();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            public static int Main(String[] args)
+            {
+                for(int i=0; i<3; i++)
+                {
+                    StartClient();
+                }
+                return 0;
+            }
+        }
+
+    }
+}
